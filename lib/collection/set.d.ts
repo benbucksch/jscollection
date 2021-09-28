7d5f448753e1ce5c1f9efc52dad00e0e45727ab0
Ben Bucksch <bbucksch@jw.org>
2021-09-28 03:25:25 +0200

    Implement SetColl using JS Set - untested

--- lib/collection/set.d.ts
+++ lib/collection/set.d.ts
@@ -2,6 +2,12 @@ import type { Collection } from '../api';
 
 declare class SetColl<Item> extends Collection<Item> {
   constructor();
+  readonly size: number;
+  delete(item: Item): void;
+  has(item: Item): boolean;
+  values(): { next: () => { value: Item, done: boolean} };
+  keys(): { next: () => { value: Item, done: boolean} };
+  entries(): { next: () => { value: Item, done: boolean} };
   private _addWithoutObserver(item: Item);
   private _removeWithoutObserver(item: Item);
 }
