5d15d210862f4044fc1e4f7c41e08e2a4a4c7f66
Ben Bucksch <ben.bucksch@beonex.com>
2024-12-15 17:59:59 +0100

    Release build script

---
+++ release.sh
@@ -0,0 +1,22 @@
+# Run from app/ directory
+[ ! -e release.sh ] && echo "Please run from root directory" 1>&2 && exit 1
+[ ! -z "`git status --porcelain`" ] && echo "Source tree not clean" 1>&2 && exit 1
+VERSION=$1
+echo Building version $VERSION
+[ -z $VERSION ] && echo "Please provide the new version number" 1>&2 && exit 1
+
+perl -p -i \
+  -e "s|\"version\": \".*\"|\"version\": \"$VERSION\"|;" \
+  package.json
+git commit package.json -m "Version $VERSION"
+git tag -s "v$VERSION" -m $VERSION
+
+NEXTVERSION=`node -e "let v = process.argv[1].split('.'); let last = parseInt(v.pop()) + 1; console.log(v.join('.') + '.' + last);" $VERSION`-dev
+perl -p -i \
+  -e "s|\"version\": \".*\"|\"version\": \"$NEXTVERSION\"|;" \
+  package.json
+git commit package.json -m "Starting version $NEXTVERSION"
+
+git push
+git push --tags
+npm publish
